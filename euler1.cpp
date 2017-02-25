61ef6b210e0e650c70d20b481fd4c0cebe3de125
Edwards <edwardsa@janelia.hhmi.org>
2017-02-25 14:04:26 -0500

    initial commit

---
+++ euler1.cpp
@@ -0,0 +1,9 @@
+//
+//  euler1.cpp
+//  
+//
+//  Created by Edwards, Austin on 2/25/17.
+//
+//
+
+#include "euler1.hpp"
---
+++ euler1.hpp
@@ -0,0 +1,14 @@
+//
+//  euler1.hpp
+//  
+//
+//  Created by Edwards, Austin on 2/25/17.
+//
+//
+
+#ifndef euler1_hpp
+#define euler1_hpp
+
+#include <stdio.h>
+
+#endif /* euler1_hpp */
